44c6055741d02b7146bb4c811f3614af7e52783c
Marco Luchi <marcoaluchi@hotmail.com>
2020-02-03 17:33:41 -0300

    cambio de metodo atacar pisando su prototipo

--- js/zombie_caminante.js
+++ js/zombie_caminante.js
@@ -38,6 +38,12 @@ ZombieCaminante.prototype.mover = function() {
   }
 }
 
+ZombieCaminante.prototype.atacar = function(){
+
+
+  jugador.perderVidas(3);
+}
+
 /* El ataque lo toma de su prototipo Enemigo que ya implementa un metodo atacar
 haciendole perder 1 vida al jugador. Si se quiere modificar el valor de ataque
 del zombie caminante habra que reimplementar este metodo desde el objeto ZombieCaminante
